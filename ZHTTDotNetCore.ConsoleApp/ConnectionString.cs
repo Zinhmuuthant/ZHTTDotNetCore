cdd4dc0205985e9925205a228f44258d434488c3
SAint <zinmhuuthant@gmail.com>
2024-04-23 15:52:24 -0700

    [Complete]Dapper CRUD

---
+++ ZHTTDotNetCore.ConsoleApp/ConnectionString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZHTTDotNetCore.ConsoleApp
+{
+    internal static class ConnectionString
+    {
+        public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "ZHTTDotNetCore",
+            UserID = "sa",
+            Password = "sa@123",
+        };
+    }
+}
--- ZHTTDotNetCore.ConsoleApp/Program.cs
+++ ZHTTDotNetCore.ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
+using System.Net.Http.Headers;
 using ZHTTDotNetCore.ConsoleApp;
 
 Console.WriteLine("Hello World");
@@ -40,13 +41,15 @@ Console.WriteLine("Hello World");
 
 //}
 
-AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
 //adoDotNetExample.Read();
 //adoDotNetExample.Create("title", "author", "content");
 //adoDotNetExample.Update(11, "test title", "test author", "test content");
 //adoDotNetExample.Delete(11);
-adoDotNetExample.Edit(11);
-adoDotNetExample.Edit(1);
+//adoDotNetExample.Edit(11);
+//adoDotNetExample.Edit(1);
+DapperExample dapperExample = new DapperExample();
+dapperExample.Run();
 
 
 Console.ReadKey();
